77ed59c03f27fe2e623b6c64c2bf3267f009f7ad
Kalvium <109285740+Kalvium-Program@users.noreply.github.com>
2023-09-22 17:20:00 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,110 @@
+let sehwagimg=document.getElementById("sehwag-img")
+let sehwag=document.getElementById("sehwag")
+sehwag.addEventListener("click",function(){
+    sehwagimg.style.display="block";
+    sehwagimg.style.transform="translate(5em,-20em)"
+    sehwagimg.onmouseleave=()=>{
+        sehwagimg.style.display="none";
+    }
+})
+
+let sachinimg=document.getElementById("sachin-img")
+let sachin=document.getElementById("sachin")
+sachin.addEventListener("click",function(){
+    sachinimg.style.display="block";
+    sachinimg.style.transform="translate(5em,-18em)"
+    sachinimg.onmouseleave=()=>{
+        sachinimg.style.display="none";
+    }
+})
+
+let gautamimg=document.getElementById("gambhir-img")
+let gautam=document.getElementById("gautam")
+gautam.addEventListener("click",function(){
+    gautamimg.style.display="block";
+    gautamimg.style.transform="translate(5em,-16em)"
+    gautamimg.onmouseleave=()=>{
+        gautamimg.style.display="none";
+    }
+})
+let viratimg=document.getElementById("virat-img")
+let virat=document.getElementById("virat")
+virat.addEventListener("click",function(){
+    viratimg.style.display="block";
+    viratimg.style.transform="translate(4em,-14em)"
+    viratimg.onmouseleave=()=>{
+        viratimg.style.display="none";
+    }
+})
+
+let dhoniImg=document.getElementById("dhoni-img")
+let dhoni=document.getElementById("dhoni")
+dhoni.addEventListener("click",function(){
+    dhoniImg.style.display="block";
+    dhoniImg.style.transform="translate(5em,-12em)"
+    dhoniImg.onmouseleave=()=>{
+        dhoniImg.style.display="none";
+    }
+})
+
+let yuvrajimg=document.getElementById("yuvraj-img")
+let yuvraj=document.getElementById("yuvraj")
+yuvraj.addEventListener("click",function(){
+    yuvrajimg.style.display="block";
+    yuvrajimg.style.transform="translate(5em,-10em)"
+    yuvrajimg.onmouseleave=()=>{
+        yuvrajimg.style.display="none";
+    }
+})
+
+let sureshimg=document.getElementById("raina-img")
+let suresh=document.getElementById("raina")
+suresh.addEventListener("click",function(){
+    sureshimg.style.display="block";
+    sureshimg.style.transform="translate(10em,0em)"
+    sureshimg.onmouseleave=()=>{
+        sureshimg.style.display="none";
+    }
+})
+
+
+let singhimg=document.getElementById("bhajan-img")
+let singh=document.getElementById("singh")
+singh.addEventListener("click",function(){
+    singhimg.style.display="block";
+    singhimg.style.transform="translate(20em,0em)"
+    singhimg.onmouseleave=()=>{
+        singhimg.style.display="none";
+    }
+})
+
+let khanimg=document.getElementById("khan-img")
+let khan=document.getElementById("khan")
+khan.addEventListener("click",function(){
+    khanimg.style.display="block";
+    khanimg.style.transform="translate(25em,0em)"
+    khanimg.onmouseleave=()=>{
+        khanimg.style.display="none";
+    }
+})
+
+
+let patelimg=document.getElementById("patel-img")
+let patel=document.getElementById("patel")
+patel.addEventListener("click",function(){
+    patelimg.style.display="block";
+    patelimg.style.transform="translate(33em,0em)"
+    patelimg.onmouseleave=()=>{
+        patelimg.style.display="none";
+    }
+})
+
+let sreeshantimg=document.getElementById("shanth-img")
+let sreeshant=document.getElementById("shanth")
+sreeshant.addEventListener("click",function(){
+    sreeshantimg.style.display="block";
+    sreeshantimg.style.transform="translate(39em,0em)"
+    sreeshantimg.onmouseleave=()=>{
+        sreeshantimg.style.display="none";
+    }
+})
